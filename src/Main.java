6d4d7f264650c98ae99ae4c4e3bc22bd99706157
lilyross <TrasH_ThrilleR@mail.ru>
2022-09-16 11:24:07 +0300

    Домашнее задание 3. 1-2-3-4-5-6-7-8-9

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,17 @@ public class Main {
         }
         System.out.println(" Задание 2 ");
         int age1 = 25;
-        if (age1 >= 7) {
-            System.out.println(" Ребенок  не ходит в школу ");
+        if (age1 >= 7 && age1 < 18) {
+            System.out.println(" Ребенок ходит в школу ");
         }
-        if (age1 >= 18) {
-            System.out.println(" Человек уже закончил школу и может отправляться в университет ");
+        if (age1 >= 18 && age1 < 24) {
+            System.out.println(" Человек ходит в университет ");
         }
         if (age1 >= 24) {
-            System.out.println(" Человек окончил университет и ему пора искать первую работу,");
+            System.out.println(" Человек ходит на работу ");
         }
         System.out.println(" Задание 3 ");
-        int vanPlace = 102;
+        int vanPlace = 45;
         if (vanPlace >= 60) {
             System.out.println(" Сидячих мест нет ");
         }
@@ -41,22 +41,13 @@ public class Main {
             System.out.println(" Информационное сообщение, что возраст совершеннолетия ещё не наступил, и нужно немного подождать. ");
         }
         System.out.println(" Задание 5 ");
-        int age3 = 14;
-        if (age3 >= 7) {
-            System.out.println(" Ребенок ходит в школу ");
-        } else {
-            System.out.println(" Ребенок не ходит в школу ");
-        }
-        if (age3 >= 18) {
-            System.out.println(" Человек уже закончил школу и может отправляться в университет ");
-        } else {
-            System.out.println(" Человек не закончил школу и не может отправлятья в университет ");
-        }
-        if (age3 >= 24) {
-            System.out.println(" Человек окончил университет и ему пора искать первую работу ");
-        } else {
-            System.out.println(" Человек еще не окончил университет и ему рано искать первую работу ");
-        }
+        int age3 = 12;
+        if (age3 >= 7 && age3 < 18) {
+            System.out.println(" Человек ходит в школу ");
+        } else if (age3 >= 18 && age3 < 24) {
+            System.out.println(" Человек ходит в университет  ");
+        } else if (age3 >= 24)
+            System.out.println(" Человек ходит на работу ");
         System.out.println(" Задание 6 ");
         int place = 101;
         if (place >= 60) {
@@ -86,11 +77,11 @@ public class Main {
             } else {
                 System.out.println(" Человек не должен ходить в школу ");
             }
-            boolean youDontGo2 = (aManOfYears >= 18 && aManOfYears < 24);
+            boolean youDontGo2 = (aManOfYears >= 18 && aManOfYears <= 24);
             if (youDontGo2) {
-                System.out.println(" Его место в университете ");
+                System.out.println(" Человеку пора в университет ");
             } else {
-                System.out.println(" Ему еще рано в университет ");
+                System.out.println(" Человеку еще рано в университет ");
             }
             boolean youDontGo3 = (aManOfYears > 24);
             if (youDontGo3) {
@@ -113,28 +104,34 @@ public class Main {
                 System.out.println("Ребенок может кататься без сопровождения взрослого");
             }
             System.out.println(" Задание 9 ");
-            int one = 1;
-            int two = 2;
-            int tree = 3;
-            boolean aLargerNumber = (one < two && one > tree);
-            if (aLargerNumber) {
-                System.out.println(one + " Большее число ");
+            int one = 10;
+            int two = -12;
+            int tree = 45;
+            if (one > two) {
+                if (one > tree) {
+                    System.out.println(" Большее число " + one);
+                } else if (tree > one) {
+                    System.out.println(" Большее число " + tree);
+                } else {
+                    System.out.println(" Числа " + one + " и " + tree + " ровны ");
+                }
+            } else if (two > one) {
+                if (two > tree) {
+                    System.out.println(" Большее число " + two);
+                } else if (tree > two) {
+                    System.out.println(" Большее число " + tree);
+                } else {
+                    System.out.println(" Числа " + two + " и " + tree + " ровны ");
+                }
             } else {
-                System.out.println( one + " Не большее число ");
-            }
-            boolean aLargerNumber1 = (two > one && two > tree);
-            if (aLargerNumber1) {
-                System.out.println(two + " Большее число ");
-            } else {
-                System.out.println( two + " Не большее число ");
-            }
-            boolean aLargerNumber2 = (tree > one && two < tree);
-            if (aLargerNumber2) {
-                System.out.println(tree + " Большее число ");
-            } else {
-                System.out.println(tree + " Не большее число ");
+                if (one > tree) {
+                    System.out.println(" Большее число " + one);
+                } else if (tree > one) {
+                    System.out.println(" Большее число " + tree);
+                } else {
+                    System.out.println(" Все числа ровны ");
+                }
             }
         }
     }
 }
-
